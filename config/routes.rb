f8830948473968a7cc86cab083d13f01c7f97efb
obanozin <obanozin18@gmail.com>
2019-11-07 09:06:53 +0000

    [add]reaction

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   
   resources :users, only: %i(show index)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reactions, only: %i(create)
 end
