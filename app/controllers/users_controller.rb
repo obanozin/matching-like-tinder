9f970dad922411ec74979255bc39b173fcbdc162
obanozin <obanozin18@gmail.com>
2019-11-07 08:01:39 +0000

    [add]swipe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-	def show
-		@user = User.find_by(id: params[:id])
-		
-	end
+  # indexアクションを追加する
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 end
